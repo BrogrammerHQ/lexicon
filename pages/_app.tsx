cad0faa46ca5bc9386b66eb0a5493ee76b54dd65
Swapnadeep Mohapatra <41564532+swapnadeepmohapatra@users.noreply.github.com>
2022-10-07 20:12:15 +0530

    [Added] basic stuff

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,22 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { NhostNextProvider, NhostClient } from "@nhost/nextjs";
+import { NhostApolloProvider } from "@nhost/react-apollo";
+
+const nhost = new NhostClient({
+  subdomain: process.env.NEXT_PUBLIC_NHOST_SUBDOMAIN || "",
+  region: process.env.NEXT_PUBLIC_NHOST_REGION || "",
+});
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <NhostNextProvider nhost={nhost} initial={pageProps?.nhostSession}>
+      <NhostApolloProvider nhost={nhost}>
+        <Component {...pageProps} />
+      </NhostApolloProvider>
+    </NhostNextProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
+export { nhost };
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,15 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import { gql, useQuery } from "@apollo/client";
+import { useUserData } from "@nhost/react";
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+import { GetServerSideProps } from "next/types";
+import styles from "../styles/Home.module.css";
+import { nhost } from "./_app";
 
-const Home: NextPage = () => {
+const Home: NextPage = ({ jargon_categories }: any) => {
+  console.log(jargon_categories);
+
   return (
     <div className={styles.container}>
       <Head>
@@ -13,60 +19,55 @@ const Home: NextPage = () => {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.tsx</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+        {jargon_categories &&
+          jargon_categories?.map((jargon_category: any) => {
+            return (
+              <div key={jargon_category.id}>
+                <Image
+                  src={jargon_category.img}
+                  alt="lol"
+                  height={200}
+                  width={200}
+                />
+                <h1>{jargon_category.title}</h1>
+              </div>
+            );
+          })}
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const GET_CATEGORIES_LIST = gql`
+    {
+      jargon_categories {
+        id
+        title
+        img
+      }
+    }
+  `;
+
+  // const nhostSession = await useQuery(GET_CATEGORIES_LIST);
+  const data = await nhost.graphql.request(GET_CATEGORIES_LIST);
+
+  if (data.error) {
+    console.log(data.error);
+    return {
+      props: {
+        error: data.error,
+      },
+    };
+  }
+
+  console.log(data.data);
+
+  return {
+    props: {
+      jargon_categories: data.data.jargon_categories,
+    },
+  };
+};
+
+export default Home;
