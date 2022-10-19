7652a600f787e5af93693f45b4cd618657303342
Swapnadeep Mohapatra <41564532+swapnadeepmohapatra@users.noreply.github.com>
2022-10-19 17:33:31 +0530

    [Fixed] env var

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -4,8 +4,12 @@ import GoogleProvider from "next-auth/providers/google";
 export const authOptions = {
   providers: [
     GoogleProvider({
-      clientId: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID || "",
-      clientSecret: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_SECRET || "",
+      clientId:
+        process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID ||
+        "616132242970-jigdv5a3qk3u2caef6lq0198sq50i17d.apps.googleusercontent.com",
+      clientSecret:
+        process.env.NEXT_PUBLIC_GOOGLE_CLIENT_SECRET ||
+        "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",
     }),
   ],
   callbacks: {
